0c7396466184b55bacf33f9d271405653d0cda91
Murat Tokak <tokakmurat01@gmail.com>
2025-07-13 13:43:55 +0300

    Add project files.

---
+++ CleanArchitecture.Presentation/Controllers/ValeusController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public sealed class ValeusController :ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Deneme api işlemi başarılı");
+        }
+    }
+}
